4c39334f1ea8a7edf09f59e34577a1326566f935
Daniel Segovia <daniel@dabici.mx>
2017-02-17 20:56:07 -0600

    sending ip to browser

--- server.js
+++ server.js
@@ -4,10 +4,13 @@ var app = express()
 var PORT = process.env.PORT || 3000;
 
 app.get('/api/whoami', function(req, res){
-	console.log(req.ip)
+	var ip = req.ip
+
+	console.log(ip)
+
+	res.end(JSON.stringify({ip: ip}))
 })
 
-
 app.listen(PORT, function(){
-	console.log('Node app is running on port', app.get('port'))
+	console.log('Node app is running on port ' + PORT)
 })
